feb659be3a7a55b9ce528a98b9d435dc9bf725e5
Antonio Carlos <antonie.dev@gmail.com>
2023-09-27 21:15:42 -0300

    finish  sidebar

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,20 +1,36 @@
-import { Search } from "lucide-react";
+import { BarChart, Flag, Home, SearchIcon, SquareStack, Users } from "lucide-react";
+import * as Input from "../Input";
 import { Logo } from "./Logo";
-import { MainNavigation } from "./MainNavigation";
+import { NavItem } from "./NavItem";
+import { Profile } from "./Profile";
+import { UsedSpaceWidget } from "./UsedSpaceWidget";
 
 export function SideBar() {
   return (
-    <aside className='border-r border-zinc-200 px-5 py-8 space-y-6'> 
+    <aside className='flex flex-col gap-6 border-r border-zinc-200 px-5 py-8 '> 
       <Logo/>
-      <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input 
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" 
-          placeholder="Search"
-        />
-      </div>
       
-      <MainNavigation />
+      <Input.Root>
+        <Input.Prefix>
+        <SearchIcon className="h-5 w-5 text-zinc-500"/>
+        </Input.Prefix>
+        <Input.Control placeholder="Search"/>
+      </Input.Root>
+      
+      
+      <nav className="space-y-0.5">
+          <NavItem title="Home" icon={Home}/>
+          <NavItem title="Dashboard" icon={BarChart}/>
+          <NavItem title="Projects" icon={SquareStack}/>
+          <NavItem title="Tasks" icon={Flag}/>
+          <NavItem title="Users" icon={Users}/>
+      </nav>
+
+      <UsedSpaceWidget />    
+
+      <div className="h-px bg-zinc-200"></div>   
+
+      <Profile />
     </aside>
   )
 }
