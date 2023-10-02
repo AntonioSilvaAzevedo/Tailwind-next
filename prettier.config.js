8739b3217d04e9db3a955090f87442aeb6408396
Antonio Carlos <antonie.dev@gmail.com>
2023-10-02 20:55:22 -0300

    add componnent inputForms

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
 };
+// module.exports = {
+//   plugins: [
+//     async () => {
+//       const tailwindPlugin = await import("prettier-plugin-tailwindcss");
+//       return tailwindPlugin.default;
+//     },
+//   ],
+// };
