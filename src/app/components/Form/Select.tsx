925641c7d4da4b7b9a599a3f0be4021c3b0ffaaf
Antonio Carlos <antonie.dev@gmail.com>
2023-10-20 10:31:10 -0300

    adding select styles

--- src/app/components/Form/Select.tsx
+++ src/app/components/Form/Select.tsx
@@ -5,19 +5,32 @@ import { Check, ChevronDown } from 'lucide-react';
 export function Select() {
   return (
     <SelectPrimitive.Root>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2">
-        <SelectPrimitive.Value className="text-black"/>
+      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 data-[placeholder]:text-zinc-600">
+        <SelectPrimitive.Value  placeholder="Select a country..." className="text-black"/>
           <SelectPrimitive.Icon>
             <ChevronDown className="h-5 w-5 text-zinc-500" />
         </SelectPrimitive.Icon>
       </SelectPrimitive.Trigger>
 
       <SelectPrimitive.Portal>
-        <SelectPrimitive.Content side='bottom' position='popper' className='z-10 rounded-lg border border-zinc-200 bg-white'>
-          <SelectPrimitive.Viewport>
-            <SelectPrimitive.Item value='br' className='flex items-center gap-2 px-3 py-2.5'>
+        <SelectPrimitive.Content 
+          side='bottom'
+          sideOffset={8} 
+          position='popper' 
+          className='z-10 rounded-lg border border-zinc-200 bg-white w-[--radix-select-trigger-width]'>
+          <SelectPrimitive.Viewport >
+            <SelectPrimitive.Item value='br' className='flex justify-between items-center gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50 overflow-hidden`'>
               <SelectPrimitive.ItemText className='text-black'>
                 Brasil
+              </SelectPrimitive.ItemText>
+              <SelectPrimitive.ItemIndicator>
+                <Check className='h-4 w-4 text-violet-500'/>
+              </SelectPrimitive.ItemIndicator>
+            </SelectPrimitive.Item>
+
+            <SelectPrimitive.Item value='us' className='flex justify-between items-center gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50'>
+              <SelectPrimitive.ItemText className='text-black'>
+                United State
               </SelectPrimitive.ItemText>
               <SelectPrimitive.ItemIndicator>
                 <Check className='h-4 w-4 text-violet-500'/>
